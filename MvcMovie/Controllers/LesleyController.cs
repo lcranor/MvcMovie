912872483b9fecaf0f336fdd1455fbc23780cf9c
Lesley Cranor <lesleycranor@gmail.com>
2017-10-01 09:09:21 -0500

    updated controllers

---
+++ MvcMovie/Controllers/LesleyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class LesleyController : Controller
+    {
+        // 
+        // GET: /Lesley/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /Lesley/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
